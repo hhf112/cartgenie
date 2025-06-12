ea7f2b95013c432f66c76b788dbd4504aed01417
harsh <hanzo4679@gmail.com>
2025-06-13 03:45:48 +0530

    polished item cards

--- client/src/content/Content.tsx
+++ client/src/content/Content.tsx
@@ -8,40 +8,41 @@ import { contentContext, type Product, type contentContextType, type contentType
 export function Content() {
   const { fetching, content, setContent } = useContext<contentContextType>(contentContext);
   return (
-      <div className="flex flex-col h-full overflow-y-auto w-full  min-w-0 overflow-x-hidden justify-start items-center  my-3 px-2">
-        {content.length ? (
-          content.map((cont: contentType) => {
-            if (cont.label == "content") {
-              return (
-                <div className="w-full my-1">
-                  <p> found {cont.products.length} result(s)!</p>
-                  <div className="flex w-full items-center rounded-2xl overflow-auto m-2">
-                    {cont.products.map(prod => <ItemCard itemName={prod.title} imageUrl={prod.imageUrl} url={prod.url} site={prod.site} />)}
-                  </div>
+    <div className="flex flex-col h-full overflow-y-auto w-full  min-w-0 overflow-x-hidden justify-start items-center  my-3 px-2">
+      {content.length ? (
+        content.map((cont: contentType) => {
+          if (cont.label == "content") {
+            return (
+              <div className="w-full my-1">
+                {/*will possibly return highest similarity score found here*/}
+                <p> here is what I found! </p>
+                <div className="flex w-full items-center rounded-2xl overflow-auto m-2">
+                  {cont.products.map(prod => <ItemCard itemName={prod.title} imageUrl={prod.imageUrl} url={prod.url} site={prod.site} />)}
                 </div>
-              )
-            }
+              </div>
+            )
+          }
 
-            else if (cont.label == "memo") {
-              return (
-                <div className="flex w-full justify-end items-start my-1">
-                  <div className="rounded-2xl bg-gray-100">
-                    <div className="flex w-full items-center rounded-xl overflow-auto m-1">
-                      {cont.imgs.map(img => <img src={img.url} className="h-25 w-25 m-2 rounded-2xl object-cover" />)}
-                    </div>
-                    <p className="text-left text-sm my-2 mx-3"> {cont.text} <br/></p>
+          else if (cont.label == "memo") {
+            return (
+              <div className="flex w-full justify-end items-start my-1">
+                <div className="rounded-2xl bg-gray-100">
+                  <div className="flex w-full items-center rounded-xl overflow-auto m-1">
+                    {cont.imgs.map(img => <img src={img.url} className="h-25 w-25 m-2 rounded-2xl object-cover" />)}
                   </div>
+                  <p className="text-left text-sm my-2 mx-3"> {cont.text} <br /></p>
                 </div>
-              )
-            }
+              </div>
+            )
+          }
 
-          })
-        ) : (
-          <h1 className="text-2xl font-semibold text-neutral-800"> Welcome to Cart Genie! </h1>
-        )
-        }
+        })
+      ) : (
+        <h1 className="text-2xl font-semibold text-neutral-800"> Welcome to Cart Genie! </h1>
+      )
+      }
 
-        {fetching && <h1 className="animate-pulse text-gray-600"> fetching results ... </h1>}
-      </div>
+      {fetching && <h1 className="animate-pulse text-gray-600"> fetching results ... </h1>}
+    </div>
   )
 }
--- client/src/content/ItemCard.tsx
+++ client/src/content/ItemCard.tsx
@@ -8,13 +8,21 @@ interface itemCardParams {
 }
 export function ItemCard({ itemName, imageUrl, url, site }: itemCardParams) {
   return (
-    <div className="min-w-1/2 h-40 flex rounded-xl justify-start items-center border border-gray-300 m-1 shadow-xl overflow-auto text-sm p-2">
+    <div className="shrink-0 w-1/2 h-40 flex rounded-xl justify-start items-center border border-gray-300 m-1 shadow-xl overflow-y-hidden overflow-x-auto text-sm p-2">
       <img src={imageUrl} className="rounded-2xl max-h-full w-2/5  m-3 object-contain" />
-      <div className="w-3/5 flex min-w-0 flex-col justify-start items-start p-2">
-        <p className="whitespace-normal break-words min-w-0 text-left font-semibold overflow-auto"> {itemName} </p>
-        <p className="  text-left whitespace-normal"> on <a href="https://amazon.com" className="text-cyan-300 underline"> {site} </a> </p>
-        <p className=" min-w-0 max-w-full my-1 text-left text-neutral-400 whitespace-normal overflow-auto,"> prices may change </p>
-        <p className=" my-1 text-left whitespace-normal"> buy now: <a className="whitespace-normal  text-green-300 underline overflow-auto" href={url} target="_blank" > link to product </a> </p>
+      <div className="w-3/5 h-full flex min-w-0 flex-col justify-start items-start p-1">
+        <p className="whitespace-normal break-words min-w-0 text-left font-semibold overflow-scroll"> {itemName} </p>
+        <p className="  text-left whitespace-normal"> 
+          <a href="https://amazon.com" className="text-blue-900 underline flex items-center overflow-auto gap-1">
+            on {site} <img src="./icons/open-link.png" className="h-3 w-3" />
+          </a> 
+        </p>
+        <p className=" min-w-0 max-w-full text-left text-neutral-400 whitespace-normal"> prices may change </p>
+        <p className=" text-left whitespace-normal">
+          <a className="whitespace-normal  flex gap-1 items-center text-blue-800 underline overflow-auto" href={url} target="_blank" >
+            buy now <img src="./icons/open-link.png" className="h-3 w-3" />
+          </a>
+        </p>
       </div>
     </div>
   )
