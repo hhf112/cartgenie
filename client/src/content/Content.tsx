85431f4a60a133c2a8c27d51f68b9a568ed3f0c5
harsh <hanzo4679@gmail.com>
2025-06-13 03:23:23 +0530

    added scroll bar to main chat

--- client/src/content/Content.tsx
+++ client/src/content/Content.tsx
@@ -8,44 +8,40 @@ import { contentContext, type Product, type contentContextType, type contentType
 export function Content() {
   const { fetching, content, setContent } = useContext<contentContextType>(contentContext);
   return (
-
-    <div className="flex grow flex-col w-full justify-end items-center m-2">
-
-      {content.length ? (
-        content.map((cont: contentType) => {
-          if (cont.label == "content") {
-            return (
-              <div className="w-full m-2">
-                <p> here is what I found! </p>
-                <div className="flex w-full items-center rounded-2xl overflow-auto m-2">
-                  {cont.products.map(prod => <ItemCard itemName={prod.title} imageUrl={prod.imageUrl} url={prod.url} site={prod.site} />)}
-                </div>
-              </div>
-            )
-          }
-
-          else if (cont.label == "memo") {
-            return (
-              <div className="flex w-full justify-end items-start m-2">
-                <div className="rounded-2xl bg-gray-100">
-                  <div className="flex w-full items-center rounded-xl overflow-auto m-1">
-                    {cont.imgs.map(img => <img src={img.url} className="h-25 w-25 m-2 rounded-2xl object-cover" />)}
+      <div className="flex flex-col h-full overflow-y-auto w-full  min-w-0 overflow-x-hidden justify-start items-center  my-3 px-2">
+        {content.length ? (
+          content.map((cont: contentType) => {
+            if (cont.label == "content") {
+              return (
+                <div className="w-full my-1">
+                  <p> found {cont.products.length} result(s)!</p>
+                  <div className="flex w-full items-center rounded-2xl overflow-auto m-2">
+                    {cont.products.map(prod => <ItemCard itemName={prod.title} imageUrl={prod.imageUrl} url={prod.url} site={prod.site} />)}
                   </div>
-                  <p className="text-left my-2 mx-3"> {cont.text} </p>
                 </div>
-              </div>
-            )
-          }
+              )
+            }
 
-        })
-      ) : (
-        <h1 className="text-2xl font-semibold text-neutral-800"> Welcome to Cart Genie! </h1>
-      )
-      }
+            else if (cont.label == "memo") {
+              return (
+                <div className="flex w-full justify-end items-start my-1">
+                  <div className="rounded-2xl bg-gray-100">
+                    <div className="flex w-full items-center rounded-xl overflow-auto m-1">
+                      {cont.imgs.map(img => <img src={img.url} className="h-25 w-25 m-2 rounded-2xl object-cover" />)}
+                    </div>
+                    <p className="text-left text-sm my-2 mx-3"> {cont.text} <br/></p>
+                  </div>
+                </div>
+              )
+            }
 
-      {fetching && <h1 className="animate-pulse text-gray-600"> fetching results ... </h1>}
+          })
+        ) : (
+          <h1 className="text-2xl font-semibold text-neutral-800"> Welcome to Cart Genie! </h1>
+        )
+        }
 
-
-    </div>
+        {fetching && <h1 className="animate-pulse text-gray-600"> fetching results ... </h1>}
+      </div>
   )
 }
