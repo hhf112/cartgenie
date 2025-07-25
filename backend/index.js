3284cdcca72c92aa545be3b38409851dfa906f0c
harsh <hanzo4679@gmail.com>
2025-07-25 16:53:45 +0530

    refactor: add embedding handling to backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import cors from 'cors';
 import fs from "fs"
 import "dotenv/config"
-import express  from 'express';
+import express from 'express';
+import FormData from 'form-data';
+import multer from "multer";
+import axios from 'axios';
 
 import { supabase } from "./db.js"
 import { pg } from "./db.js";
+
 const app = express();
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 
 try {
@@ -21,26 +27,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const search = async (req, res) => {
-  try {
-    const embedding = req.body.embeddings;
-    
-    const result = await pg.query(`SELECT url, caption, product_url FROM products ORDER BY noindex <-> '[${embedding}]' LIMIT 5;`);
-
-    res.status(200).json(result)
-    console.log("request served")
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("request failed")
-  }
-};
 
 app.get('/', (req, res) => {
-  res.status(200).send("working")
+  res.status(200).json({status: "working"});
 })
 
-app.post('/upload', search);
+app.post('/upload', upload.single('images'), async function(req, res, next) {
+  try {
+    const form = new FormData();
+    form.append("images", req.file);
+    form.append("text", req.body.text);
+    form.append("session", req.body.session);
 
+    const response = await axios.post(`${process.env.EMBEDADDR}/upload`, form, {
+      headers: {
+        ...form.getHeaders(),
+      },
+    });
+
+    const embedding = response.data;
+
+    const result = await pg.query(`SELECT url, caption, product_url FROM products ORDER BY noindex <-> '[${embedding}]' LIMIT 5;`);
+
+    // console.log(embedding);
+    // console.log(result);
+
+    res.status(200).json(result);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error." });
+  }
+})
 
 app.listen(PORT, () => {
   console.log(PORT);
